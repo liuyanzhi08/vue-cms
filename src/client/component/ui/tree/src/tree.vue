992e74d8975f39305ee9a1912de91a6f840d2421
liuyanzhi <liuyanzhi@gf.com.cn>
2017-12-27 18:57:21 +0800

    tree: finish styles

--- src/client/component/ui/tree/src/tree.vue
+++ src/client/component/ui/tree/src/tree.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>test</div>
+    <div class="ui-tree">
+        <div class="parent">
+            <div class="name">src</div>
+            <div class="parent">
+                <div class="name">client</div>
+                <div class="parent">
+                    <div class="name">api</div>
+                    <div class="leaf">a.js</div>
+                    <div class="parent">
+                        <div class="name">api-sub</div>
+                        <div class="leaf">api-sub-a.js</div>
+                    </div>
+                </div>
+                <div class="leaf">index.js</div>
+            </div>
+            <div class="parent">server</div>
+        </div>
+        <div class="leaf">README.md</div>
+    </div>
 </template>
 <script>
     class Pagigation {
@@ -35,5 +53,15 @@
     }
 </script>
 <style lang="scss">
-
+    .ui-tree {
+        .parent > * {
+            margin-left: 30px;
+        }
+        .leaf {
+            margin-left: 60px;
+        }
+        > .parent > *, > .leaf {
+            margin-left: 0;
+        }
+    }
 </style>
