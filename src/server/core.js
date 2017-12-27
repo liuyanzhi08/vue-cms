b1f76b84b2d512e1c671554fdb2a81cfe56648d7
liuyanzhi <liuyanzhi@gf.com.cn>
2017-12-27 11:36:31 +0800

    add browser-sync support

--- src/server/core.js
+++ src/server/core.js
@@ -4,9 +4,11 @@ import router from './router'
 import KoaWebpack from 'koa-webpack'
 import Webpack from 'webpack'
 import config from '../../webpack.config.js'
+import BrowserSync from 'browser-sync'
 
-const compiler = Webpack(config);
+const compiler = Webpack(config)
 const app = new Koa()
+const browserSync = BrowserSync.create()
 
 app
     .use(KoaWebpack({
@@ -16,4 +18,14 @@ app
     .use(router.routes())
     .use(router.allowedMethods())
 
-app.listen(1991)
+app.listen(1993)
+browserSync.init({
+    port: 1991,
+    ui: {
+        port: 1992
+    },
+    proxy: 'localhost:1993',
+    files: ['**/*']
+}, function () {
+    console.log('browser refreshed.')
+})
