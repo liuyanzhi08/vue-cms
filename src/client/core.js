b6373ba440c13d6a7b052c6ee5ad72df0131495d
liuyanzhi08 <702368372@qq.com>
2018-09-03 22:10:22 +0800

    add elemment-ui

--- src/client/core.js
+++ src/client/core.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
+import ElementUI from 'element-ui';
 import Ui from './component/ui/index'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
 Vue.use(Vuex)
+Vue.use(ElementUI)
 Vue.use(Ui)
 
 // Vue.use(Directive)
