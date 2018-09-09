75e156c938644ea39f07f86b1b2c9c647f98ae54
liuyanzhi08 <702368372@qq.com>
2018-09-10 02:58:18 +0800

    add back file-loader

--- scripts/stop.js
+++ scripts/stop.js
@@ -2,5 +2,5 @@ require("babel-register");
 require("babel-polyfill");
 
 const stop = require('kill-port');
-const port = require('../src/server/config').server.port;
+const port = require('../src/config').server.port;
 stop(port);
