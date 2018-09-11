aa7c4aea8660c387052c5dd94d3f5ca3d4c9d157
liuyanzhi <liuyanzhi@gf.com.cn>
2018-09-11 13:50:02 +0800

    add todo. I'm going to work now

--- src/server/component/staticize.js
+++ src/server/component/staticize.js
@@ -3,6 +3,7 @@ import { savePageRecurse } from '../helper/spider';
 
 export default {
   get: async (ctx) => {
+    // tod need auth
     await savePageRecurse(`${server.url}${path.user}`, path.static, 'index.html');
     ctx.body = { msg: 'success' };
   },
