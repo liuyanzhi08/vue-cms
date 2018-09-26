0e2d20ccf79f883adbb8b6354280460023b625a9
liuyanzhi08 <702368372@qq.com>
2018-09-27 02:19:49 +0800

    fix db migrate

--- src/server/db/knex/migration/20180926152208_article.js
+++ src/server/db/knex/migration/20180926152208_article.js
@@ -10,8 +10,8 @@ exports.up = knex => knex.schema.createTable(articleTable, (table) => {
   table.text('summary').nullable();
   table.integer('category_id').unsigned().nullable();
   table.foreign('category_id').references(`${categoryTable}.id`).onDelete('CASCADE');
-  table.timestamp('created_at');
-  table.timestamp('updated_at').defaultTo(knex.fn.now());
+  table.dateTime('created_at', 6).notNullable().defaultTo(knex.fn.now(6));
+  table.timestamp('updated_at', 6);
 });
 
 exports.down = knex => knex.schema.dropTable(articleTable);
