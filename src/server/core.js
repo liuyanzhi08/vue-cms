0371c7e0e718a50f05d131eda370bc0c7d223f66
liuyanzhi <liuyanzhi@gf.com.cn>
2018-11-05 16:44:02 +0800

    [ds] back end finish hot-reload

--- src/server/core.js
+++ src/server/core.js
@@ -4,11 +4,13 @@ import koaCompress from 'koa-compress';
 import session from 'koa-session';
 import opn from 'opn';
 import ip from 'ip';
+import chokidar from 'chokidar';
+import path from 'path';
 import router from './router';
 import passport from './passport';
 import { log } from './helper/logger';
 import { isDev } from './helper/env';
-import { ssr, server } from './config';
+import { ssr, server, dir } from './config';
 import spaDevServer from '../../script/spa-dev-server';
 import ssrDevServer from '../../script/ssr-dev-server';
 
@@ -38,9 +40,21 @@ class Server {
 
     if (isDev) {
       app.$devServer = setupDevServer(app);
-      log('wait for webpack finish building...');
-      app.$devServer.then(() => log('webpack finished building'));
-      opn(`http://${ip.address()}:${server.port}/admin`);
+      log('[DS] wait for webpack finish building...');
+      app.$devServer.then(() => log('[DS] webpack finished building'));
+      const watcher = chokidar.watch(path.join(dir.root, '/src'), { ignored: /client/ });
+      watcher.on('ready', () => {
+        log('[FW] file watcher ready');
+        opn(`http://${ip.address()}:${server.port}/admin`);
+        watcher.on('all', (e, p) => {
+          log(`[FW] ${p} ${e}`);
+          Object.keys(require.cache).forEach((id) => {
+            if (/vue-cms\/src/.test(id)) {
+              delete require.cache[id];
+            }
+          });
+        });
+      });
     }
 
     return app;
