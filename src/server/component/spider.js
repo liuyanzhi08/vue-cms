f74dffc8c82cdb1b621e7aab2ef12d11b5cebc89
liuyanzhi <liuyanzhi@gf.com.cn>
2019-02-28 11:56:34 +0800

    finish spider

--- src/server/component/spider.js
+++ src/server/component/spider.js
@@ -1,15 +1,11 @@
 import Joi from 'joi';
 import axios from 'axios';
 import cheerio from 'cheerio';
-import fse from 'fs-extra';
 import Turndown from 'turndown';
-import config from '../config';
 import ctxHelper from '../helper/ctx';
-import ssrHelper from '../helper/ssr';
 import regHelper from '../helper/reg';
 import urlHelper from '../helper/url';
 import error from '../helper/error';
-import env from '../helper/env';
 import netHelper from '../helper/net';
 import article from '../models/article';
 import logger from '../helper/logger';
@@ -55,6 +51,69 @@ const schema = Joi.object().keys({
   }),
 });
 
+const grabListArticles = async (
+  url, categoryId, listSelector, detailSelector, detailTheme,
+) => {
+  const siteRoot = urlHelper.root(url);
+  const res = await axios.get(url);
+  const $ = cheerio.load(res.data);
+  const links = $(`${listSelector}`);
+  const articles = Array.from(links).map((item) => {
+    const articleUrl = item.attribs.href.indexOf('http') === -1
+      ? `${siteRoot}${item.attribs.href}` : item.attribs.href;
+    return {
+      url: articleUrl,
+      title: $(item).text(),
+    };
+  });
+  const successArticleUrls = [];
+  const failedArticleUrls = [];
+  await util.asyncForEach(articles, async (articleData) => {
+    let detailRes;
+    try {
+      detailRes = await axios.get(articleData.url);
+      successArticleUrls.push(articleData.url);
+      log(`spider: success to fetch '${articleData.url}'`);
+    } catch (e) {
+      failedArticleUrls.push(articleData.url);
+      err(`spider: fail to fetch '${articleData.url}'`);
+      return;
+    }
+    const detailHtml = detailRes.data;
+    const $detail = cheerio.load(detailHtml, {
+      decodeEntities: false,
+      normalizeWhitespace: true,
+    });
+    const content = $detail(detailSelector).html();
+    const newContent = await grabImg(content, siteRoot);
+    await article.save({
+      title: articleData.title,
+      category_id: categoryId,
+      content: turndown.turndown(newContent),
+      theme: detailTheme,
+    });
+  });
+  return {
+    successArticleUrls,
+    failedArticleUrls,
+  };
+};
+
+const listUrlReg = /\$range\((\d+),\s*(\d+)\)/i;
+const parseListUrl = (url) => {
+  const urls = [];
+  const matched = url.match(listUrlReg);
+  if (!matched) {
+    urls.push(url);
+  } else {
+    const start = +matched[1];
+    const end = +matched[2];
+    for (let i = start; i < end; i += 1) {
+      urls.push(url.replace(listUrlReg, i));
+    }
+  }
+  return urls;
+};
 
 class Spider {
   async post (ctx) {
@@ -74,52 +133,22 @@ class Spider {
     const listSelector = data.list.selector;
     const detailSelector = data.detail.selector;
     const detailTheme = data.detail.theme;
-    const siteRoot = urlHelper.root(listUrl);
 
-    const res = await axios.get(listUrl);
-    const $ = cheerio.load(res.data);
-    const links = $(`${listSelector}`);
-    const articles = Array.from(links).map((item) => {
-      const articleUrl = item.attribs.href.indexOf('http') === -1
-        ? `${siteRoot}${item.attribs.href}` : item.attribs.href;
-      return {
-        url: articleUrl,
-        title: $(item).text(),
-      };
+    let allSuccessArticleUrls = [];
+    let allFailedArticleUrls = [];
+    const listUrls = parseListUrl(listUrl);
+    await util.asyncForEach(listUrls, async (url) => {
+      const { successArticleUrls, failedArticleUrls } = await grabListArticles(
+        url, categoryId, listSelector, detailSelector, detailTheme,
+      );
+      allSuccessArticleUrls = allFailedArticleUrls.concat(successArticleUrls);
+      allFailedArticleUrls = allFailedArticleUrls.concat(failedArticleUrls);
     });
-    if (!articles.length) {
+    if (!allSuccessArticleUrls.length && !allFailedArticleUrls.length) {
       fail(ctx, error.spiderListLinkNotFound);
       return;
     }
-    const successArticleUrls = [];
-    const failedArticleUrls = [];
-    let failedImgUrls;
-    await util.asyncForEach(articles, async (articleData) => {
-      let detailRes;
-      try {
-        detailRes = await axios.get(articleData.url);
-        successArticleUrls.push(articleData.url);
-        log(`spider: success to fetch '${articleData.url}'`);
-      } catch (e) {
-        failedArticleUrls.push(articleData.url);
-        err(`spider: fail to fetch '${articleData.url}'`);
-        return;
-      }
-      const detailHtml = detailRes.data;
-      const $detail = cheerio.load(detailHtml, {
-        decodeEntities: false,
-        normalizeWhitespace: true,
-      });
-      const content = $detail(detailSelector).html();
-      const newContent = await grabImg(content, siteRoot);
-      await article.save({
-        title: articleData.title,
-        category_id: categoryId,
-        content: turndown.turndown(newContent),
-        theme: detailTheme,
-      });
-    });
-    success(ctx, { successArticleUrls, failedArticleUrls });
+    success(ctx, { allSuccessArticleUrls, allFailedArticleUrls });
   }
 }
 
